d0692b4b49711c096f61e2f68245683870076058
Janssen <jjanssen@docker.dyn.datasys.swri.edu>
2019-12-17 14:02:44 -0600

    Target and background classes added Max range for inference Demo video mode Fcn8 with imagenet weights

--- pcs_detection/scripts/masking_inference.py
+++ pcs_detection/scripts/masking_inference.py
@@ -33,8 +33,11 @@ import cv2 as cv
 import json
 import os
 
+import tensorflow as tf
+from keras.backend.tensorflow_backend import set_session
+
 # Inference module
-from pcs_detection.inference import Inference
+from src_python.pcs_detection.inference import Inference
 
 # Hack because code was originally written to have configs as Python modules
 class Config:
@@ -42,10 +45,14 @@ class Config:
         self.__dict__.update(entries)
 
 if __name__ == '__main__':
+  tf_config = tf.ConfigProto()
+  tf_config.gpu_options.allow_growth = True
+  sess = tf.Session(config=tf_config)
+  set_session(sess)
   # Import Config json file and convert into format we need
   dir_path = os.path.dirname(os.path.realpath(__file__))
   
-  with open(dir_path + '/data/weights/<weight_path>/inference_config.json') as json_data_file:
+  with open(dir_path + '/data/weights/Hood_label_fcn8_LAB_19_12_09_190846/full_config.json') as json_data_file:
       data = json.load(json_data_file)
   config = Config(**data)
 
@@ -53,24 +60,23 @@ if __name__ == '__main__':
   annotator = Inference(config)
 
   # Load the image
-  input_image = cv.imread('<image you want to test>')
+  input_image = cv.imread('/mnt/project_share/Rars/new_val_4/positives/visible/frame0597.jpg')
+
   # Generate the annotation and convert to 3 channel image
   res = annotator.make_prediction(input_image)
 
-  display_image =  np.zeros((res.shape[0], res.shape[1], 3))
-  colors = [(102, 204, 255), (255, 153, 102), (0,153,255)]
-  for jj, color in enumerate(colors):
-        temp_chnl_img = np.zeros((res.shape[0],res.shape[1], 2))
-        temp_chnl_img[:,:,1][res==jj+1] = 1
-        display_image[temp_chnl_img[:,:,1]==1] = color
-  display_image = display_image.astype(np.uint8)
+  print('after')
+  print(res.max(), res.min())
+
+  results_image = cv.merge((res, res, res)) * 255
+  results_image = results_image.astype(np.uint8)
 
   # Show the results
   print("Input image of size: " + str(input_image.shape))
-  print("Results image of size: " + str(display_image.shape))
+  print("Results image of size: " + str(results_image.shape))
   print("Press ESC to exit")
   while True:
-      cv.imshow("image", np.hstack((input_image, display_image)))
+      cv.imshow("image", np.hstack((input_image, results_image)))
       k = cv.waitKey(1) & 0xFF
       if k == 27:
           cv.destroyWindow("image")
